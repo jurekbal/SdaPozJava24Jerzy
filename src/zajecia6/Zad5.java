396ff52745b3ad307d6d121291db241d89485bbf
Jerzy Balwinski <jurek_b@wp.pl>
2020-02-15 12:33:45 +0100

    working Zaj6 zad5

--- src/zajecia6/Zad5.java
+++ src/zajecia6/Zad5.java
@@ -8,9 +8,19 @@ public class Zad5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Podaj liczbę:");
-        String liczbaStr = sc.nextLine();
+        System.out.print("Podaj liczbę:");
+        int liczba = sc.nextInt();
+        int sumaCyfr = 0;
 
-        //liczbaStr.split();
+        int rozbierana = liczba;
+        while (rozbierana > 0) {
+            sumaCyfr += rozbierana % 10;
+            rozbierana /= 10;
+        }
+
+        System.out.println("Suma cyfr liczby: " + liczba + " wynosi: " + sumaCyfr);
+
+        //        String liczbaStr = sc.nextLine();
+//        liczbaStr.split();
     }
 }
