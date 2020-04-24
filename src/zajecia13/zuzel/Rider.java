c8bb8370fb6e8ec164b2b245acc86fa1d439e93c
Jurek Balwiński <jurek_b@wp.pl>
2020-04-24 11:00:39 +0200

    Zad Dom po zaj 13 - żużel (w trakcie) 3 pierwsze funkcje, 3 do poprawki

--- src/zajecia13/zuzel/Rider.java
+++ src/zajecia13/zuzel/Rider.java
@@ -4,13 +4,16 @@ public class Rider implements Runnable {
     private static int numOfRiders = 0;
 
     private final String name;
-    private final int startPositionNum;
+    private final int riderNumber; // nr zawodnika na torze
     private final double maxSpeed; // m/s
 
     private double currentSpeed;
     private int currentLap = 1;
+    private int lastLap = 1;
     private boolean finishPassed = false;
-    private double positionOnTrack = 0;
+    private double currentPositionOnTrack = 0;
+    private double lastPositionOnTrack = 0;
+    //private double totalDistance = 0;
     private int lastLapMillis = 0;
     private int totalRideMillis = 0;
     private final int TRACK_LENGTH = Track.TRACK_LENGTH;
@@ -20,20 +23,50 @@ public class Rider implements Runnable {
 
     public Rider(String name, int maxSpeed) {
         this.name = name;
-        this.startPositionNum = ++numOfRiders;
+        this.riderNumber = ++numOfRiders;
         this.maxSpeed = maxSpeed * 1000.0 / 3600.0; // jednostka parametru: km/h
         this.currentSpeed = this.maxSpeed; // jednostka pola: m/s
     }
 
     @Override
     public void run() {
-        System.out.println(name + " (" + startPositionNum + ") rozpczął bieg.");
+        System.out.println(name + " (" + riderNumber + ") rozpczął bieg.");
 
         while (!finishPassed) {
             sleepOneTick();
             updateTimesAndPosition();
+            checkIfOvertaken();
+
             checkIfLapPassed();
         }
+
+    }
+
+    private void checkIfOvertaken() {
+        //TODO poprawić, zmienić algorytm, bo nie wykrywa dublowania w każdym przypadku - najlepiej zmienić
+        //TODO algo na zupełnie inny!
+        //TODO + obsługa sytuacji, gdy riderzy mijają się w momencie przekraczania linii okrążenia
+        for (Rider rival : Track.riders) {
+            if (rival.getRiderNumber() != riderNumber ) { // check if not me
+                if (checkPositionPasss(rival)) {
+                    if (rival.getCurrentLap() == rival.getLastLap()) {
+                        // tu jest minięcie bez przekroczenia mety
+                        if (amILapAhead(rival)) {
+                            System.out.println(name + " zdublował " + rival.name);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean amILapAhead(Rider rival) {
+        return currentLap > rival.getCurrentLap();
+    }
+
+    private boolean checkPositionPasss(Rider rival) {
+        return lastPositionOnTrack < rival.getLastPositionOnTrack()
+                && currentPositionOnTrack > rival.getCurrentPositionOnTrack();
     }
 
     private void sleepOneTick() {
@@ -45,13 +78,13 @@ public class Rider implements Runnable {
     }
 
     private void checkIfLapPassed() {
-        if (positionOnTrack > TRACK_LENGTH) {
-            System.out.println(name + " (" + startPositionNum + ") czas okrążenia nr " + currentLap + ": "
+        if (currentPositionOnTrack > TRACK_LENGTH) {
+            System.out.println("["+riderNumber+"] " + name + ": czas okrążenia nr " + currentLap + ": "
                     + (lastLapMillis / 1000.0) + "s");
-            currentLap++;
+            lastLap = currentLap++;
             if (currentLap > 4) {
                 finishPassed = true;
-                System.out.println(name + " (" + startPositionNum + ") zakończył wyścig z czasem " +
+                System.out.println("["+riderNumber+"] " + name + ": zakończył wyścig z czasem " +
                         (totalRideMillis / 1000.0) + "s" );
             }
             resetPositionAndLapTime();
@@ -60,16 +93,38 @@ public class Rider implements Runnable {
 
     private void resetPositionAndLapTime() {
         // zerowanie pozycji po okrążeniu z uwaględnieniem odcinka przejechanego do czasu obliczeń
-        positionOnTrack = positionOnTrack % TRACK_LENGTH;
+        currentPositionOnTrack = currentPositionOnTrack % TRACK_LENGTH;
         // zerowanie czasu okrążęnia z uwzględnieniem pozycji w momencie obliczeń
-        lastLapMillis = (int) (currentSpeed / positionOnTrack);
+        lastLapMillis = (int) (currentSpeed / currentPositionOnTrack);
     }
 
     private void updateTimesAndPosition() {
         lastLapMillis += TICK_MILLIS;
         totalRideMillis += TICK_MILLIS;
-        positionOnTrack += currentSpeed / TICKS_PER_SECOND;
+        lastPositionOnTrack = currentPositionOnTrack;
+        double positionDelta = currentSpeed / TICKS_PER_SECOND;
+        currentPositionOnTrack += positionDelta;
+        //totalDistance += positionDelta;
     }
 
+    public int getRiderNumber() {
+        return riderNumber;
+    }
+
+    public int getCurrentLap() {
+        return currentLap;
+    }
+
+    public int getLastLap() {
+        return lastLap;
+    }
+
+    public double getCurrentPositionOnTrack() {
+        return currentPositionOnTrack;
+    }
+
+    public double getLastPositionOnTrack() {
+        return lastPositionOnTrack;
+    }
 }
 
--- src/zajecia13/zuzel/Track.java
+++ src/zajecia13/zuzel/Track.java
@@ -5,14 +5,15 @@ import java.util.List;
 
 public class Track {
     public static final int TRACK_LENGTH = 370; // m
+    public static List<Rider> riders;
 
     public static void main(String[] args) {
-        List<Rider> riders = new ArrayList<>();
+        riders = new ArrayList<>();
 
-        riders.add(new Rider("Gollob", 115));
-        riders.add(new Rider("Czaja", 95));
+        riders.add(new Rider("Gollob", 140));
+        riders.add(new Rider("Czaja", 45));
         riders.add(new Rider("Gomólski", 101));
-        riders.add(new Rider("Lampart", 89));
+        riders.add(new Rider("Lampart", 68));
 
         for (Rider rider : riders) {
             Thread thread = new Thread(rider);
